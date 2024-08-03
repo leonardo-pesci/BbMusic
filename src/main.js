da4e3bd60c7b698658a561222dbd271c3eb2d6e6
leonardo-pesci <leonardo.pesci.2003@gmail.com>
2024-08-03 21:41:55 +0200

    footer

--- src/main.js
+++ src/main.js
@@ -1,3 +1,124 @@
+//^========================================================================
+//^                            ELEMENTI
+//^========================================================================
+const navBtns = document.querySelectorAll('.navBtn');
+const mainSections = document.querySelectorAll('.mainSection')
+
+//^========================================================================
+//^                            VARIABILI
+//^========================================================================
+
+//^========================================================================
+//^                            FUNZIONI
+//^========================================================================
+let setLastSection = (lastSection) => {
+    localStorage.setItem('lastSection', JSON.stringify(lastSection))
+}
+
+let showSection = (item) => {
+        let lowerItem = item.toLowerCase()
+        
+        mainSections.forEach( (section) => {
+            section.classList.add('hidden')
+        })
+
+        setLastSection(item)
+
+        const section = document.querySelector(`#${lowerItem}`)
+        section.classList.remove('hidden')
+}
+
+showSection(lastSection)
+
+// L'observer
+let options = {
+	root: null,
+	rootMargin: '0px',
+	threshold: 1,
+}
+
+let callback = (entries, observer) => {
+	objData = entries[0];
+	
+	if (objData.isIntersecting) {
+        counters.forEach( (element) => {
+            startCounters(element);
+        })
+
+		observer.unobserve(objData.target);
+	}
+}
+
+let observer = new IntersectionObserver(callback, options);
+
+counters.forEach( (element) => {
+    observer.observe(element);
+})
+
+let startCounters = (counter) => {
+
+    let value = 0
+    let end = parseInt(counter.dataset.to)
+    let duration = parseInt(counter.dataset.duration)
+
+    let step = duration / 100
+    let fraction = end / 100
+    
+    let timer = setInterval( () => {
+        value = value + fraction
+        
+        if (value >= end) {
+            clearInterval(timer) 
+            value = end
+        }
+        
+        let response = ''
+
+        switch (counter.id) {
+            case 'people':
+                response = Math.floor(value)
+                break;
+
+            case 'money':
+                response = '€' + Math.floor(value / 100) + '.' + Math.floor(value % 100)
+                break;
+        
+            case 'percentage':
+                response = Math.floor(value) + '%'
+                break;
+        
+            default:
+                break;
+        }
+
+        counter.innerHTML = response
+
+    }, step);
+    
+}
+
+document.querySelector('#' + lastSection + 'Btn').style.color = 'white'
+
+//^========================================================================
+//^                             EVENTI
+//^========================================================================
+navBtns.forEach( (item) => {
+    item.addEventListener('click', () => {
+        let name = item.id.replace('Btn', '')
+        navBtns.forEach( (btn) => {
+            btn.style.color = 'var(--color-2)'
+        })
+        item.style.color = 'white'
+        
+        showSection(name)
+    })
+})
+
+
+
+
+
+
 // section menu active
 function onScroll(event) {
     const sections = document.querySelectorAll(".menu-scroll");
@@ -48,3 +169,4 @@ function showPanel(panelIndex) {
     tabPanels[panelIndex].style.display = "flex";
 }
 showPanel(0);
+
